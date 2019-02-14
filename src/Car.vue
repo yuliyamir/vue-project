d1983eff257c96def47bfa0478a02bf63b6b1710
Yuliya Miroshnichenko <lyulyamir@gmail.com>
2019-02-14 17:56:41 +0300

    Компоненты. Передача параметров компоненту. Валидация входящих параметров.

---
+++ src/Car.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="car">
+      <h3>Name: {{ carName }} \ {{ reverseName }}</h3>
+      <p>Year: {{ carYear }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+      name: "Car",
+      // props: ['carName', 'carYear'],
+      // props: {
+      //   carName: String,
+      //   carYear: Number
+      // },
+      props: {
+        carName: {
+          type: String,
+          required: true,
+          default: 'Default name'
+        },
+        carYear: Number
+      },
+      computed: {
+        reverseName() {
+          return this.carName.split('').reverse().join('')
+        }
+      }
+    }
+</script>
+
+<style scoped>
+  .car {
+    border: 2px solid #e1d5a8;
+    padding: 10px 20px;
+  }
+</style>
