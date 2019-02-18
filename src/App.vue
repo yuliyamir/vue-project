9330ed92faddb925e5d1f759f47d5426ffe4f396
Yuliya Miroshnichenko <lyulyamir@gmail.com>
2019-02-18 17:12:46 +0300

    Жизненный цикл директивы

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
-    <h2 v-colored>{{ title }}</h2>
+    <h2 v-colored v-if="visible">{{ title }}</h2>
+
+    <button @click="visible = !visible">Toggle</button>
+    <button @click="title = 'New title'">Change title</button>
   </div>
 </template>
 
@@ -11,7 +14,8 @@ export default {
   name: 'app',
   data() {
     return {
-      title: 'Hello I am Vue!'
+      title: 'Hello I am Vue!',
+      visible: true
     }
   }
 }
--- src/color.js
+++ src/color.js
@@ -1,5 +1,23 @@
 export default {
   bind(el, bindings, vnode) {
+    console.log('bind');
     el.style.color = 'red';
-  }
+  },
+
+  inserted(el, bindings, vnode) {
+    console.log('inserted');
+  },
+
+  update(el, bindings, vnode) {
+    console.log('update');
+  },
+
+  componentUpdated(el, bindings, vnode) {
+    console.log('componentUpdated');
+  },
+
+  unbind(el, bindings, vnode) {
+    console.log('unbind');
+  },
+
 }
