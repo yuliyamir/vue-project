ca7448a5608654cb18e91a3aa0c89e15fb74148e
ymiroshnichenko <lyulyamir@gmail.com>
2019-04-22 17:03:28 +0300

    Настройка валидаторов

--- src/App.vue
+++ src/App.vue
@@ -7,20 +7,31 @@
                     type="email"
                     id="email"
                     class="form-control"
+                    @blur="$v.email.$touch()"
                     v-model="email"
                 >
             </div>
+
+            <pre>{{$v.email}}</pre>
         </form>
     </div>
 </template>
 
 <script>
+import { required, email } from 'vuelidate/lib/validators'
+
  export default {
  	data () {
  		return {
  			email: ''
         }
-    }
+    },
+     validations: {
+ 		email: {
+			required,
+			email
+        }
+     }
  }
 </script>
 
