b1e8ef4ef141d4ab7a3a1dbd731559f384b0d0bc
yuliya miroshnichenko <lyulyamir@gmail.com>
2019-05-06 15:12:24 +0300

    vue-router: Создание навигации

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,18 @@
 <template>
     <div class="container">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="collapse navbar-collapse">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <router-link class="nav-link" to="/">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/cars">Cars</router-link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
         <router-view></router-view>
     </div>
 </template>
