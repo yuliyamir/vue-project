b0dade85b36e34ba376a0408f8cbf42193d4d407
yuliya miroshnichenko <lyulyamir@gmail.com>
2019-05-06 16:37:29 +0300

    vue-router: хэш и скролл

--- src/pages/CarFull.vue
+++ src/pages/CarFull.vue
@@ -2,6 +2,12 @@
 	<div>
     <h4>Car name: {{ $route.query.name }}</h4>
     <h5>Car year: {{ year }}</h5>
+
+    <div class="scroll"></div>
+
+    <h6 id="scroll">Hello from the bottom</h6>
+
+    <div class="scroll"></div>
   </div>
 </template>
 
@@ -17,5 +23,9 @@
 </script>
 
 <style scoped>
-
+  .scroll {
+    width: 100px;
+    height: 1000px;
+    background-color: #ccc;
+  }
 </style>
